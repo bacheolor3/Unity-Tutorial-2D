f19907f4941a9bdc7e53211b203bebae307eabc5
Kwon <bacheolor3@gmail.com>
2025-05-20 10:58:20 +0900

    [ADD] Unity Project
    -- Asset
    -- Packages
    -- ProjectSettings

---
+++ Assets/02. Scripts/Movement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // this.transform.position = this.transform.position + Vector3.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.position = this.transform.position + Vector3.forward * moveSpeed;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+    }
+}
